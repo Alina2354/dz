62995c7b470689857d5ab24d7bcd3b9b6b56fa8a
alina akunishnikova <akunishnikovaalina@gmail.com>
2025-07-14 23:40:26 +0300

    add function

--- dz.py
+++ dz.py
@@ -1,5 +1,33 @@
-def get_vowels(String):
-    return [each for each in String if each in "aeiou"]
-get_vowels("animal") # [a, i, a]
-get_vowels("sky") # []
-get_vowels("football") # [o, o, a]
+def count_sales(sales):
+    product_revenue = {}
+    for product, quantity, price in sales:
+        revenue = quantity * price
+        if product in product_revenue:
+            product_revenue[product] += revenue
+        else:
+            product_revenue[product] = revenue
+    return product_revenue
+
+def count_max_sales(product_revenue):
+    best_selling_product = None
+    max_revenue = 0
+    for product, revenue in product_revenue.items():
+        if revenue > max_revenue:
+            max_revenue = revenue
+            best_selling_product = product
+    print(f"Товар с максимальной выручкой: {best_selling_product} (Выручка: {max_revenue})")
+
+
+
+if __name__ == '__main__':
+    sales = [
+        ("Ноутбук", 5, 50000),
+        ("Смартфон", 10, 30000),
+        ("Наушники", 20, 2000),
+        ("Ноутбук", 3, 48000),
+        ("Смартфон", 7, 32000),
+        ("Наушники", 15, 2500)
+    ]
+    product_revenue = count_sales(sales)
+    print(product_revenue)
+    print(count_max_sales(product_revenue))
