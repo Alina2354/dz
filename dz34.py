de0b29178c80e745a109d9f7051ae671fa1e0e3a
alina akunishnikova <akunishnikovaalina@gmail.com>
2025-09-09 15:53:15 +0300

    add 2 class

--- dz34.py
+++ dz34.py
@@ -9,3 +9,31 @@ class Vehicle:
 
     def __str__(self):
         return self.get_info()
+
+
+class Car(Vehicle):
+    def __init__(self, brand, model, year, num_doors):
+        super().__init__(brand, model, year)
+        self.num_doors = num_doors
+
+    def get_info(self):
+        return f"{super().get_info()}, {self.num_doors} doors"
+
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, engine_capacity):
+        super().__init__(brand, model, year)
+        self.engine_capacity = engine_capacity
+
+    def get_info(self):
+        return f"{super().get_info()}, {self.engine_capacity} engine"
+
+
+if __name__ == '__main__':
+    car = Car("Toyota", "Camry", 2022, 4)
+    bike = Bike("Honda", "CBR600RR", 2021, 599)
+
+    print(car.get_info())
+    print(bike.get_info())
+
+    print(car)
+    print(bike)
