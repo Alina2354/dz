dae9634aa715888d15b14c6a7721572b5227f255
alina akunishnikova <akunishnikovaalina@gmail.com>
2025-09-09 15:50:02 +0300

    add function

--- dz34.py
+++ dz34.py
@@ -3,3 +3,9 @@ class Vehicle:
         self.brand = brand
         self.model = model
         self.year = year
+
+    def get_info(self):
+        return f"{self.brand} {self.model} ({self.year})"
+
+    def __str__(self):
+        return self.get_info()
