e3e04e83daa8633a04432edca279d4fb2ad04869
alina akunishnikova <akunishnikovaalina@gmail.com>
2025-09-11 11:44:32 +0300

    change function

--- dz37.py
+++ dz37.py
@@ -1,5 +1,14 @@
 def divide(numerator, denominator):
+    if not isinstance(numerator, (int, float)):
+        raise TypeError("Numerator must be a number (int or float)")
+    if not isinstance(denominator, (int, float)):
+        raise TypeError("Denominator must be a number (int or float)")
+    if denominator == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
     return numerator / denominator
+
 if __name__ == '__main__':
+
     result1 = divide(10, 2)
     print(f"10 / 2 = {result1}")
+
